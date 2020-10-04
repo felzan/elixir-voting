bc9f3f1153ba6642b8a5562463b23cd3f9b140db
Luis Felipe Zanatto <luisfelipe.zanatto@gmail.com>
2020-10-04 15:29:15 -0300

    Add Admin authentication

---
+++ lib/voting_web/controllers/admin/session_controller.ex
@@ -0,0 +1,18 @@
+defmodule VotingWeb.Admin.SessionController do
+  use VotingWeb, :controller
+
+  alias Voting.SignInAdmin
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    case SignInAdmin.run(email, password) do
+      {:ok, admin} ->
+        render(conn, "session.json", %{admin: admin})
+
+      {:error, _} ->
+        conn
+        |> put_status(401)
+        |> json(%{status: "Unauthorized"})
+    end
+
+  end
+end
